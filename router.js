268574a40ac28a19dab996989f552503967e80c9
VerenaSchwachmeyer <Vronus@gmx.de>
2023-03-28 14:03:24 +0200

    added endpoints for users database

--- router.js
+++ router.js
@@ -12,18 +12,56 @@ router.get("/user", async (req, res) => {
   }
 });
 
-router.get("/user/:id"),
-  async (req, res) => {
-    const { id } = req.params;
-    try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [
-        id,
-      ]);
-      res.json({ data: rows });
-    } catch (err) {
-      res.sendStatus(404);
-      // use adequate error messages and numbers.
-    }
-  };
+//get user with certain id
+router.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id=$1;", [id]);
+    res.json({ data: rows });
+  } catch (err) {
+    res.sendStatus(404);
+    // use adequate error messages and numbers.
+  }
+});
 
+//create a new user
+router.post("/user", async (req, res) => {
+  const { first_name, last_name, age } = req.body;
+  try {
+    const { rows } = await pool.query(
+      "INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *",
+      [first_name, last_name, age]
+    );
+    res.json({ data: rows });
+  } catch (e) {
+    res.sendStatus(403);
+  }
+  res.end();
+});
+
+//to edit one user with the id
+router.put("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const { first_name } = req.body;
+  try {
+    const { rows } = await pool.query(
+      "UPDATE users SET first_name=$1 WHERE id=$2 RETURNING *",
+      [first_name, id]
+    );
+    res.json({ data: rows });
+  } catch (e) {
+    res.sendStatus(403);
+  }
+});
+
+// delete an id
+router.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query("DELETE FROM users WHERE id=$1", [id]);
+    res.json({ message: `You have been deleted from our database. ID: ${id}` });
+  } catch (err) {
+    res.sendStatus(404);
+  }
+});
 module.exports = router;
