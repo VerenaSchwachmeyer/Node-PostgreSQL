33f55eb48fe5656d735a965b6b00f7eb5091e191
VerenaSchwachmeyer <Vronus@gmx.de>
2023-03-28 16:40:18 +0200

    separated routes for users and orders into two files. solved first extra

--- router.js
+++ router.js
@@ -64,87 +64,20 @@ router.delete("/user/:id", async (req, res) => {
     res.sendStatus(404);
   }
 });
+
+/////////////EXTRAS
+//EXTRA: create a user route that will return all the orders of a user
+router.get("/user/:id/orders", async (req, res) => {
+  const { id } = req.params.id;
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM orders WHERE user_id=$1;",
+      [req.params.id]
+    );
+    res.json({ data: rows });
+  } catch (err) {
+    res.sendStatus(404);
+  }
+});
+
 module.exports = router;
-
-/////// ORDERS
-// get all orders
-router.get("/order", async (req, res) => {
-  try {
-    const { rows } = await pool.query("SELECT * from orders");
-    res.json({ data: rows });
-  } catch (err) {
-    res.sendStatus(500);
-  }
-});
-
-//get order with id
-router.get("/order/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const { rows } = await pool.query("SELECT * FROM orders WHERE id=$1;", [
-      id,
-    ]);
-    res.json({ data: rows });
-  } catch (err) {
-    res.sendStatus(404);
-    // use adequate error messages and numbers.
-  }
-});
-
-//create a new order
-router.post("/order/", async (req, res) => {
-  const { price, date, user_id } = req.body;
-  try {
-    const { rows } = await pool.query(
-      "INSERT INTO orders (price, date, user_id ) VALUES ($1, $2, $3) RETURNING *",
-      [price, date, user_id]
-    );
-    res.json({ data: rows });
-  } catch (e) {
-    res.sendStatus(403);
-  }
-  res.end();
-});
-
-//to edit one orfer with the id
-router.put("/order/:id", async (req, res) => {
-  const { id } = req.params;
-  const { price } = req.body;
-  try {
-    const { rows } = await pool.query(
-      "UPDATE orders SET price=$1 WHERE id=$2 RETURNING *",
-      [price, id]
-    );
-    res.json({ data: rows });
-  } catch (e) {
-    res.sendStatus(403);
-  }
-});
-
-// delete an order with an id
-router.delete("/order/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const { rows } = await pool.query("DELETE FROM orders WHERE id=$1", [id]);
-    res.json({
-      message: `Your purchase has been removed from the shopping cart.`,
-    });
-  } catch (err) {
-    res.sendStatus(404);
-  }
-});
-
-//EXTRA: create a user route taht will return all the orders of a user
-router.get("user/:id/orders", async (req, res) => {
-  console.log("test hello");
-  const { id } = req.params;
-  //   try {
-  //     const { rows } = await pool.query(
-  //       "SELECT * FROM orders WHERE user_id=$1;",
-  //       [id]
-  //     );
-  //     res.json({ data: rows });
-  //   } catch (err) {
-  //     res.sendStatus(404);
-  //   }
-});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require("./router");
+const routerOrders = require("./routerOrders");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,5 +11,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", router);
+app.use("/api", routerOrders);
 
 module.exports = app;
