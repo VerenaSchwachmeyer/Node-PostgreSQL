f52cfed46e6796c0ff925bc5396362e494cd2062
VerenaSchwachmeyer <Vronus@gmx.de>
2023-03-28 12:55:30 +0200

    second query: id. implemented but ThunderClient returns error. still trying to fix.

--- router.js
+++ router.js
@@ -3,14 +3,27 @@ const pool = require("./db");
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/user", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * from users");
     res.json({ data: rows });
   } catch (err) {
-    console.log(err);
     res.sendStatus(500);
   }
 });
+
+router.get("/user/:id"),
+  async (req, res) => {
+    const { id } = req.params;
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [
+        id,
+      ]);
+      res.json({ data: rows });
+    } catch (err) {
+      res.sendStatus(404);
+      // use adequate error messages and numbers.
+    }
+  };
 
 module.exports = router;
